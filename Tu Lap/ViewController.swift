419dc4fc9ab910593eca501ea8b1ad712ebb7e7a
Phuoc <phuocpeter19@gmail.com>
2016-03-24 19:56:55 -0400

    Initial Commit

---
+++ Tu Lap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tu Lap
+//
+//  Created by Tran Thai Phuoc on 2016-03-24.
+//  Copyright © 2016 Tran Thai Phuoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
