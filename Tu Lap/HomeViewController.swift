4c060bcb2ef72668e3fd6365613549ef1a13986d
Phuoc <phuocpeter19@gmail.com>
2016-03-24 20:31:28 -0400

    Minor Interface

--- Tu Lap/HomeViewController.swift
+++ Tu Lap/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class HomeViewController: UITableViewController {
 
+  // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,16 @@ class HomeViewController: UITableViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  // MARK: - Table View Delegate Methods
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
 
+  // MARK: - IBAction
+  @IBAction func refresh() {
+    
+  }
 
 }
 
---
+++ Tu Lap/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Tu Lap
+//
+//  Created by Tran Thai Phuoc on 2016-03-24.
+//  Copyright © 2016 Tran Thai Phuoc. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+  @IBOutlet weak var name: UILabel!
+
+}
